f87924631611fe47c053934301a987c37a9aeed0
ashwini-8 <ashwinip309@gmail.com>
2020-12-17 22:42:35 +0530

    Added:FirstName Regex use case

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,11 +1,22 @@
 package com.project.regex;
 
-public class UserRegistration {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+    private static final String  FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
     public static void main(String[]args)
     {
         System.out.println("User Registration regex");
+        System.out.println("Enter your first name");
+        Scanner input=new Scanner(System.in);
+        String firstName=input.next();
+        firstNameRegex(firstName);
+    }
 
+    public static void firstNameRegex(String firstName){
+        boolean Result = firstName.matches(FIRSTNAME);
+        System.out.println("\nResult " + Result + "First Name: " + firstName );
     }
 
 
