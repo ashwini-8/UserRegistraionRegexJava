0857de0c29da61bf7a206023ab57285292f17b72
ashwini-8 <ashwinip309@gmail.com>
2020-12-17 23:47:31 +0530

    Added:Email Regex use case

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     private static final String  FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$";
     private static final String  LASTNAME = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String  EMAIL =  "^[A-Za-z0-9+_.-]+@(.+)$";
     static Scanner input=new Scanner(System.in);
 
     public static void main(String[]args)
@@ -13,18 +14,21 @@ public class UserRegistration {
         chooseOption();
     }
     public static void chooseOption(){
-        System.out.println("Options are: \n 1.First-Name    2.Last-Name");
+        System.out.println("Options are: \n 1.First-Name    2.Last-Name     3.Email-id");
         System.out.println("Enter option to check Regex Pattern");
-
-         int inputOption = input.nextInt();
+        int inputOption = input.nextInt();
          switch (inputOption){
              case 1 : System.out.println("Enter your first name");
                       String firstName=input.next();
                       firstNameRegex(firstName);
-             break;
-             case 2 : System.out.println("Enter your last name");
+                      break;
+             case 2 : System.out.println("Enter your Last name");
                  String lastName=input.next();
-                 firstNameRegex(lastName);
+                 lastNameRegex(lastName);
+                 break;
+             case 3 : System.out.println("Enter your Email-id");
+                 String Email=input.next();
+                 emailIdRegex(Email);
                  break;
              default:
                  System.out.println("Enter a valid Option");
@@ -35,8 +39,12 @@ public class UserRegistration {
         System.out.println("\nResult " + Result + "\nFirst Name: " + firstName );
     }
     public static void lastNameRegex(String lastName){
-        boolean Result = lastName.matches(FIRSTNAME);
+        boolean Result = lastName.matches(LASTNAME);
         System.out.println("\nResult " + Result + "\nLast Name: " + lastName );
+    }
+    public static void emailIdRegex(String email){
+        boolean Result = email.matches(EMAIL);
+        System.out.println("\nResult " + Result + "\nEmail: " + email );
     }
 }
 
